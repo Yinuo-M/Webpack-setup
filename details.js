4cecb2e097b1c194d18184aa4c1b6e77c6a5d383
Yinuo-M <mengyn1999@gmail.com>
2021-04-12 12:10:41 +0100

    Change lodash to lodash-es

--- details.js
+++ details.js
@@ -1,11 +1,13 @@
 import "./details.css";
+//NOTE lodash is a special case where, in order to tree shake, we must import it without using destructuring. For more details see https://developers.google.com/web/fundamentals/performance/optimizing-javascript/tree-shaking 
+import debounce from "lodash-es/debounce";
 
 async function getComponent() {
 	const element = document.createElement("div");
 	//NOTE below is an example of code-splitting
 	const { default: _ } = await import(
 		//NOTE below is an example of webpack smart comment, so we can name modules that are dynamically imported.
-		/* webpackChunkName: "lodash" */ "lodash/array"
+		/* webpackChunkName: "lodash" */ "lodash-es/array"
 	);
 
 	element.innerHTML = _.join(["Hello", "webpack"], " ");
@@ -21,7 +23,7 @@ if (module.hot) {
 	module.hot.accept();
 }
 
-//NOTE the code below sets up the service worker in production mode only. Only one file per web page need to have this function.
+//NOTE the code below sets up the service worker in production mode only. Only one file per website need to have this function.
 
 if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
